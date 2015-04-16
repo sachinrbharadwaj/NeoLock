7e82a73eedb7c118ae1f41db4368de25983687de
sachinrbharadwaj <sachinrbharadwaj@gmail.com>
2015-04-16 22:46:37 +0530

    Building v1.0 progress

--- mobile/src/main/java/com/codermaster/neolock/LockscreenActivity.java
+++ mobile/src/main/java/com/codermaster/neolock/LockscreenActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.codermaster.neolock.model.KeyData;
+
+import java.util.Date;
 
 
 public class LockscreenActivity extends ActionBarActivity {
@@ -12,28 +19,66 @@ public class LockscreenActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lockscreen);
+
+        final KeyData keyData = new KeyData();
+        Button redButton = (Button) findViewById(R.id.buttonRed);
+        Button blueButton = (Button) findViewById(R.id.buttonBlue);
+        Button greenButton = (Button) findViewById(R.id.buttonGreen);
+        Button yellowButton = (Button) findViewById(R.id.buttonYellow);
+        Button clearButton = (Button) findViewById(R.id.buttonClear);
+        final TextView displayText = (TextView) findViewById(R.id.displayText);
+
+        final String key = "bbrrg";
+
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyData.setUncodedKey(keyData.getUncodedKey()+"r");
+                if(keyData.getUncodedKey().equalsIgnoreCase(key))
+                    displayText.setText("SUCCESS.....");
+                else
+                    displayText.setText(keyData.getUncodedKey());
+            }
+        });
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyData.setUncodedKey(keyData.getUncodedKey()+"b");
+                if(keyData.getUncodedKey().equalsIgnoreCase(key))
+                    displayText.setText("SUCCESS.....");
+                else
+                    displayText.setText(keyData.getUncodedKey());
+            }
+        });
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyData.setUncodedKey(keyData.getUncodedKey()+"g");
+                if(keyData.getUncodedKey().equalsIgnoreCase(key))
+                    displayText.setText("SUCCESS.....");
+                else
+                    displayText.setText(keyData.getUncodedKey());
+            }
+        });
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyData.setUncodedKey(keyData.getUncodedKey()+"y");
+                if(keyData.getUncodedKey().equalsIgnoreCase(key))
+                    displayText.setText("SUCCESS.....");
+                else
+                    displayText.setText(keyData.getUncodedKey());
+            }
+        });
+
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                keyData.setUncodedKey("");
+                displayText.setText(keyData.getUncodedKey());
+            }
+        });
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lockscreen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ mobile/src/main/java/com/codermaster/neolock/model/KeyData.java
@@ -0,0 +1,15 @@
+package com.codermaster.neolock.model;
+
+/**
+ * Created by sachin.bharadwaj on 16-Apr-15.
+ */
+public class KeyData {
+    private String uncodedKey = "";
+
+    public String getUncodedKey() {
+        return uncodedKey;
+    }
+    public void setUncodedKey(String uncodedKey) {
+        this.uncodedKey = uncodedKey;
+    }
+}
